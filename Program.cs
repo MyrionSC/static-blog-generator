ce9ee8083e9cd1f8b69e86ff38177a9df139dce0
Marand <marpht23@gmail.com>
2021-10-16 11:05:29 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace static_blog_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
