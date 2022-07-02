a80d6b01b53820f841d0e14d6fa1da948216ae42
Marand <marpht23@gmail.com>
2022-07-02 13:41:12 +0200

    building html part 1

--- Helpers/ContentHelper.cs
+++ Helpers/ContentHelper.cs
@@ -8,6 +8,11 @@ public static class ContentHelper
     public static string CreateFrontPageHtmlContent(List<ParsedFile> parsedFinanceFileList)
     {
         return $@"
+
+
+
+
+
 <h1>Here we go</h1>
 {parsedFinanceFileList.Select(f => $"<p><a href=\"{f.MetaData.UrlPath}\">{f.MetaData.Title}</a></p>").StringJoin("\n")}\n
 aa";
