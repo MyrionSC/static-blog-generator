ec7879a14863803039ea721fc9b66c1683c6ad42
Marand <marpht23@gmail.com>
2022-07-02 14:01:28 +0200

    forgot some stuff

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public static class Extensions
+{
+    public static string StringJoin(this IEnumerable<string> list, string separator)
+    {
+        string result = string.Join(separator, list);
+        return result;
+    }
+}
