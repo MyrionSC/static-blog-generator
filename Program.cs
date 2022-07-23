277d3ac270b88a317f85a5406c1a27270cb7408f
Marand <marpht23@gmail.com>
2022-07-23 17:09:55 +0200

    tech to frontpage

--- Program.cs
+++ Program.cs
@@ -17,15 +17,25 @@ namespace static_blog_generator
         {
             GoogleCredential credential = DriveHelper.GetGoogleCreds();
             var fileList = await DriveHelper.GetFileList(credential);
+            
             var financeFileList = DriveHelper.GetDirFilesByParentId(fileList, FINANCE_DIR_ID);
-
             var parsedFinanceFileList = financeFileList.Select(f => DriveHelper.ParseFile(f, credential)).ToList();
+            parsedFinanceFileList.Sort((a, b) => a.MetaData.Date < b.MetaData.Date ? 1 : -1 );
+            
+            var techFileList = DriveHelper.GetDirFilesByParentId(fileList, TECH_DIR_ID);
+            var parsedTechFileList = techFileList.Select(f => DriveHelper.ParseFile(f, credential)).ToList();
+            parsedTechFileList.Sort((a, b) => a.MetaData.Date < b.MetaData.Date ? 1 : -1 );
 
-            string frontpageHtmlContent = ContentHelper.CreateFrontPageHtmlContent(parsedFinanceFileList);
+            string frontpageHtmlContent = ContentHelper.CreateFrontPageHtmlContent(parsedFinanceFileList, parsedTechFileList);
 
             Directory.CreateDirectory("publish");
             await File.WriteAllTextAsync("publish/index.html", frontpageHtmlContent);
             foreach (ParsedFile parsedFile in parsedFinanceFileList) {
+                Directory.CreateDirectory($"publish/{parsedFile.MetaData.UrlPath}");
+                await File.WriteAllTextAsync($"publish/{parsedFile.MetaData.UrlPath}/index.html",
+                    parsedFile.HtmlContent);
+            }
+            foreach (ParsedFile parsedFile in parsedTechFileList) {
                 Directory.CreateDirectory($"publish/{parsedFile.MetaData.UrlPath}");
                 await File.WriteAllTextAsync($"publish/{parsedFile.MetaData.UrlPath}/index.html",
                     parsedFile.HtmlContent);
