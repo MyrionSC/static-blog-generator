c108b205d3323c4eb3e9bc00745f6bddd0d4e7d3
Marand <marpht23@gmail.com>
2022-07-23 17:05:29 +0200

    insert finance list in frontpage

--- Helpers/ContentHelper.cs
+++ Helpers/ContentHelper.cs
@@ -6,7 +6,16 @@ namespace static_blog_generator;
 public static class ContentHelper
 {
     public static string CreateFrontPageHtmlContent(List<ParsedFile> parsedFinanceFileList)
-    { 
+    {
+        var me = parsedFinanceFileList.Select(f => f.MetaData.UrlPath).StringJoin("\n");
+        var financeListItems = parsedFinanceFileList.Select(f => 
+            $$"""
+            <li class="post-item">
+                <div class="text-nowrap mr05rem">{{f.MetaData.Date:yyyy-MM-dd}}</div>
+                <span><a href="{{f.MetaData.UrlPath}}">{{f.MetaData.Title}}</a></span>
+            </li>
+            """
+        ).StringJoin("\n");
         return $$"""""
                 <!DOCTYPE html>
                 <html lang="en">
@@ -29,14 +38,7 @@ public static class ContentHelper
                         <div style="flex: 1">
                             <span class="h1">Finance</span>
                             <ul class="post-list">
-                                <li class="post-item">
-                                    <div class="text-nowrap mr05rem">2021-06-27</div>
-                                    <span><a href="https://www.takuzen.me/hugo-theme-cactus/posts/code-block-test/">Code Block Test</a></span>
-                                </li>
-                                <li class="post-item">
-                                    <div class="text-nowrap mr05rem">2021-06-07</div>
-                                    <span><a href="https://www.takuzen.me/hugo-theme-cactus/posts/2020-06-06-changes-in-java-string/">Changes to String in java (from 1.7.0_06)</a></span>
-                                </li>
+                                {{financeListItems}}
                             </ul>
                         </div>
                         <div style="flex: 1">
