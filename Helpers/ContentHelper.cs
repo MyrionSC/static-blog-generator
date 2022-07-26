f519a823a32d1af623a30cf6a16400558334bef9
Marand <marpht23@gmail.com>
2022-07-26 17:44:26 +0200

    finance -> business

--- Helpers/ContentHelper.cs
+++ Helpers/ContentHelper.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Google.Apis.Drive.v3.Data;
 
 namespace static_blog_generator;
 
 public static class ContentHelper
 {
-    public static string CreateFrontPageHtmlContent(List<ParsedFile> parsedFinanceFileList, List<ParsedFile> parsedTechFileList)
+    public static string CreateFrontPageHtmlContent(List<ParsedFile> parsedBusinessFileList, List<ParsedFile> parsedTechFileList)
     {
-        var financeListItems = parsedFinanceFileList.Select(f => 
+        var businessListItems = parsedBusinessFileList.Select(f => 
             $$"""
             <li class="post-item">
                 <div class="text-nowrap mr05rem">{{f.MetaData.Date:yyyy-MM-dd}}</div>
@@ -31,22 +30,21 @@ public static class ContentHelper
                     <meta charset="UTF-8">
                     <title>Title</title>
                     <meta name="viewport" content="width=device-width,initial-scale=1">
-                    <link rel=preload href="../static/JetBrainsMono-Regular.woff2" as=font type=font/woff2>
-                    <link rel="stylesheet" href="../static/style.css">
+                    <link rel=preload href="static/JetBrainsMono-Regular.woff2" as=font type=font/woff2>
+                    <link rel="stylesheet" href="static/style.css">
                 </head>
                 <body class="max-width mx-auto px3 ltr">
                 <div class="content index py4">
                     <section id="about">
-                        Hugo is a general-purpose website framework. Technically speaking, Hugo is a static site generator. Unlike
-                        systems that dynamically build a page with each visitor request, Hugo builds pages when you create or update
-                        your content. Since websites are viewed far more often than they are edited, Hugo is designed to provide an
-                        optimal viewing experience for your website’s end users and an ideal writing experience for website authors.
+                        Velkommen til. This is my blog. There are many like it, but this one is mine. I write about my 
+                        experience running a one-man software consulting business in Denmark, as a born and raised 
+                        native dane, as well as whatever technical subject that catches my fancy.
                     </section>
                     <section class="d-me-flex flex-gap-1rem">
                         <div style="flex: 1">
-                            <span class="h1">Finance</span>
+                            <span class="h1">Business</span>
                             <ul class="post-list">
-                                {{financeListItems}}
+                                {{businessListItems}}
                             </ul>
                         </div>
                         <div style="flex: 1">
--- Program.cs
+++ Program.cs
@@ -3,41 +3,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Google.Apis.Auth.OAuth2;
 using DriveFile = Google.Apis.Drive.v3.Data.File;
+// ReSharper disable InconsistentNaming
 
 namespace static_blog_generator
 {
-    class Program
+    internal static class Program
     {
         // If modifying these scopes, delete your previously saved credentials
         // at ~/.credentials/docs.googleapis.com-dotnet-quickstart.json
-        private const string FINANCE_DIR_ID = "1hfnLBQfz0QRRNag-HdL0noOA0T6Xnl_9";
+        private const string BUSINESS_DIR_ID = "1hfnLBQfz0QRRNag-HdL0noOA0T6Xnl_9";
         private const string TECH_DIR_ID = "1F6IsmhCroPVLZh0rp6Gn_DsGO06GLhFz";
 
-        static async Task Main(string[] args)
+        private static async Task Main()
         {
             GoogleCredential credential = DriveHelper.GetGoogleCreds();
             var fileList = await DriveHelper.GetFileList(credential);
             
-            var financeFileList = DriveHelper.GetDirFilesByParentId(fileList, FINANCE_DIR_ID);
-            var parsedFinanceFileList = financeFileList.Select(f => DriveHelper.ParseFile(f, credential)).ToList();
-            parsedFinanceFileList.Sort((a, b) => a.MetaData.Date < b.MetaData.Date ? 1 : -1 );
+            var businessFileList = DriveHelper.GetDirFilesByParentId(fileList, BUSINESS_DIR_ID);
+            var parsedBusinessFileList = businessFileList.Select(f => DriveHelper.ParseFile(f, credential)).ToList();
+            parsedBusinessFileList.Sort((a, b) => a.MetaData.Date < b.MetaData.Date ? 1 : -1 );
             
             var techFileList = DriveHelper.GetDirFilesByParentId(fileList, TECH_DIR_ID);
             var parsedTechFileList = techFileList.Select(f => DriveHelper.ParseFile(f, credential)).ToList();
             parsedTechFileList.Sort((a, b) => a.MetaData.Date < b.MetaData.Date ? 1 : -1 );
 
-            string frontpageHtmlContent = ContentHelper.CreateFrontPageHtmlContent(parsedFinanceFileList, parsedTechFileList);
+            string frontpageHtmlContent = ContentHelper.CreateFrontPageHtmlContent(parsedBusinessFileList, parsedTechFileList);
 
-            Directory.CreateDirectory("publish");
-            await File.WriteAllTextAsync("publish/index.html", frontpageHtmlContent);
-            foreach (ParsedFile parsedFile in parsedFinanceFileList) {
-                Directory.CreateDirectory($"publish/{parsedFile.MetaData.UrlPath}");
-                await File.WriteAllTextAsync($"publish/{parsedFile.MetaData.UrlPath}/index.html",
+            await File.WriteAllTextAsync("index.html", frontpageHtmlContent);
+            foreach (ParsedFile parsedFile in parsedBusinessFileList) {
+                Directory.CreateDirectory($"{parsedFile.MetaData.UrlPath}");
+                await File.WriteAllTextAsync($"{parsedFile.MetaData.UrlPath}/index.html",
                     parsedFile.HtmlContent);
             }
             foreach (ParsedFile parsedFile in parsedTechFileList) {
-                Directory.CreateDirectory($"publish/{parsedFile.MetaData.UrlPath}");
-                await File.WriteAllTextAsync($"publish/{parsedFile.MetaData.UrlPath}/index.html",
+                Directory.CreateDirectory($"{parsedFile.MetaData.UrlPath}");
+                await File.WriteAllTextAsync($"{parsedFile.MetaData.UrlPath}/index.html",
                     parsedFile.HtmlContent);
             }
         }
